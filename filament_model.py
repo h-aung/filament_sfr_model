9e447aecdf1c8b42f24a956ea4c7d755a2710a8d
h-aung <hansoeaung9@gmail.com>
2024-05-02 16:06:02 +0300

    added pdf, notebook and updates

--- filament_model.py
+++ filament_model.py
@@ -4,9 +4,8 @@ from scipy import interpolate as interp
 from astropy import units as u
 from astropy import constants as c
 from scipy import optimize as opt
-import customlib as cl
-
-from astropy.cosmology import Planck15 as cosmo
+#import customlib as cl
+from scipy.integrate import solve_ivp
 
 def safelog(f):
     if hasattr(f, '__array__'):
@@ -139,141 +138,218 @@ class Cooling(two_d_table):
             
 C=Cooling(file='CIE_cool_M5.txt',cooling_nH2=True)
 
-def tcooltff(radius, halo_mass, z, cosmo, s = 1.2, metallicity = 0.):
-    if s=="derive":
-        s = ((3*cosmo.age(z)/2)*(2.8*((1+z)/8)**1.5)/u.Gyr).to('')
-    delta_from_s = interp.interp1d([1.5,5],[5,50],fill_value='extrapolate')
-    cosmic_fraction = cosmo.Ob0/cosmo.Om0
-    #radius = np.logspace(-2,0) ### unit less
-    density = delta_from_s(s)*cosmo.critical_density(z)*cosmo.Om(z)/radius**(1.62)
-    filament_mass = 0.03*((halo_mass/1e12)**0.14)*((1+z)**2.5)*halo_mass*u.Msun/(cl.vcirc(halo_mass, z, 'vir',cosmo) * u.Gyr)   /3
-
-    gamma = 5./3
-    mu = 0.61
-    virial_T = ((c.G * filament_mass*(mu*c.m_p))/c.k_B).to('K')
-    virial_radii = np.sqrt(filament_mass*0.34/(2*np.pi*density[-1]))
-    mass = 2*np.pi*density[-1]*(radius**0.34)*virial_radii**2 *cosmic_fraction/ 0.34
-    tcool = c.k_B*virial_T / \
-    ((gamma-1)*(density*cosmic_fraction/(mu*c.m_p))* C.Lambda_units(metallicity,virial_T)) 
-
-    tff = radius*virial_radii/np.sqrt(c.G * mass)
-    return (tcool/tff).to(''), (mass/filament_mass/cosmic_fraction).to('')
-    
-    
-def tcooltff_isothermal(radius, halo_mass, z, s = 1.2, metallicity = 0.):
-    if s=="derive":
-        s = ((3*cosmo.age(z)/2)*(2.8*((1+z)/8)**1.5)/u.Gyr).to('')
-    delta_from_s = interp.interp1d([1.5,5],[5,50],fill_value='extrapolate')
-    cosmic_fraction = cosmo.Ob0/cosmo.Om0
-    #radius = np.logspace(-2,0) ### unit less
-    filament_mass = 0.03*((halo_mass/1e12)**0.14)*((1+z)**2.5)*halo_mass*u.Msun/(cl.vcirc(halo_mass, z, 'vir',cosmo) * u.Gyr)   /3
-    gamma = 5./3
-    mu = 0.61
-    virial_T = ((c.G * filament_mass*(mu*c.m_p))/c.k_B).to('K')
-    density = delta_from_s(s)*cosmo.critical_density(z)*cosmo.Om(z)/radius**(1.62)
-    virial_radii = np.sqrt(filament_mass*0.34/(2*np.pi*density[-1]))
-    rhoc = filament_mass / ( (32./57) * np.pi * virial_radii**2 )
-    cs = np.sqrt(gamma*c.k_B*virial_T/(mu*c.m_p))
-    H = 1*virial_radii
-    
-    density = rhoc*(1+ (190./128)*radius**2 )**(-2)
-    
-    mass = np.pi * rhoc * cosmic_fraction *virial_radii**2 * (radius**2/ (1+ 25.*radius**2/ 32.))
-    tcool = c.k_B*virial_T / \
-    ((gamma-1)*(density*cosmic_fraction/(mu*c.m_p))* C.Lambda_units(metallicity,virial_T)) 
-    tff = radius*virial_radii/np.sqrt(c.G * mass)
-    return (2*tcool/tff).to(''), (mass/filament_mass/cosmic_fraction).to('')
-
-from colossus.halo import mass_defs
-from colossus.cosmology import cosmology
-from colossus.halo import concentration, mass_so, profile_nfw
-cosmo_colossus = cosmology.setCosmology('planck15')
-from colossus.lss import peaks
-import colossus
-from scipy.integrate import quad
-
-def Gamma(c_nfw):
-    return 1.15 + 0.01*(c_nfw - 6.5)
-
-def eta0(c_nfw):
-    return 0.00676*(c_nfw - 6.5)**2 + 0.206*(c_nfw - 6.5) + 2.48
-
-def NFWPhi(r, M, z, c, mass_def='vir'):
-    R = mass_so.M_to_R(M, z, mass_def)
-    if(type(r) != np.ndarray and r == 0):
-        return -1. * (G * M / R) * (c / NFWf(c))
+def virial_values_filament(M, z, cosmo, mdef = '40m', quantity='pressure', mu = 0.59):
+    '''Calculate the virial/normalized values for different quantities of halos.
+    Parameters:
+       M: filament mass per unit length in Msun/kpc unit
+       z: redshift
+       cosmo: astropy cosmology object
+          Example: from astropy.cosmology import Planck15 as cosmo
+       mdef: overdensity mass definition
+          xxxc or xxxm (xxx is any integer), e.g. 200c
+          virial temperature and velocity does not depend on mdef unlike halo
+       quantity: virial quantity to calculate, 
+          Choices are radius, density, pressure, temperature, velocity, time.
+          radius: physical radius in kpc.
+          density: average total density in g/cm^3. Multiply with cosmo.Ob0/cosmo.Om0 for baryonic density
+          temperature: mu*mp*( G*M*(2/3) ) in keV. Divide by Boltzmann constant for temperature. See Lu+2024 and filament_model.pdf.
+          pressure: rho_gas * G*M*(2/3) in keV/cm^3.
+          velocity: sqrt(G*M) in km/s.
+          time: radius/velocity in Myr. Dynamical/virial crossing time independent of filament mass, and only on mdef and redshift.
+          Multiply with 2 for time to return to the virial radius, 2*pi for orbital time
+       mu: mean particle weigh. Only used in tempreature.
+    Return:
+       virial quantity. Can use .to('xx') to convert to desired units. See astropy documentations.
+       Default units are radius(kpc), P(keV/cm3), T(keV), v(km/s), t(Myr)
+    '''
+    try:
+        if not M.unit.is_equivalent(u.Msun/u.kpc):
+            raise ValueError('wrong Mv unit')
+    except AttributeError:
+        M = M*u.Msun/u.kpc
+    rho_crit = cosmo.critical_density(z)
+    if mdef[-1] == 'c':
+        delta = int(mdef[:-1])
+        rho = delta*rho_crit
+    elif mdef[-1] == 'm':
+        delta = int(mdef[:-1])
+        rho = delta*rho_crit*cosmo.Om(z)
     else:
-        return -1. * (G * M / R) * (c / NFWf(c)) * (np.log(1. + c*r/R) / (c*r/R))
-
-# this now agrees with Komatsu and Seljak eqn 19 for theta
-# the confusion was that rho0 / P0 is 3*eta0^-1 and then units removed by scaling by R/GM
-def theta(r, M, z, c, mass_def='vir'):
-    R = mass_so.M_to_R(M, z, mass_def)
-    # the rho0/P0 is actually 3eta^-1(0) * R/(GM) from Komatsu and Seljak
-    rho0_by_P0 = 3*eta0(c)**-1 * R/(G*M)
-    return 1. + ((Gamma(c) - 1.) / Gamma(c))*rho0_by_P0*(NFWPhi(0, M, z, c, mass_def='vir')-NFWPhi(r, M, z, c, mass_def='vir'))
-
-def rho0(M, z, c = None, conc_model='diemer19', mass_def='vir'):
-    if c is None:
-        c = concentration.concentration(M, mass_def, z, model=conc_model)
-    R = mass_so.M_to_R(M, z, mass_def)
-    nume = cbf * M
-    denom = 4. * np.pi * quad(lambda r: theta(r, M, z, c, mass_def=mass_def)**(1.0 / (Gamma(c) - 1.0)) * r**2, 0, R)[0]
-    return nume/denom
-
-def rho_gas(r, M, z, c = None, conc_model='diemer19', mass_def='vir'):
-    if c is None:
-        c = concentration.concentration(M, mass_def, z, model=conc_model)
-        return rho0(M, z, c=c, mass_def=mass_def) * theta(r, M, z, c, mass_def=mass_def)**(1.0 / (Gamma(c) - 1.0)), c
+        raise ValueError("Unsupported mdef")
+    fb = cosmo.Ob0/cosmo.Om0
+    R = np.sqrt(M/(np.pi * rho)).to('kpc')
+    if quantity=='radius':
+        return R
+    elif quantity=='density':
+        return rho    
+    elif quantity=='pressure':
+        return ( fb * rho * c.G * M /3 ).to('keV/cm3')
+    elif quantity=='temperature':
+        return ( c.G * M * mu * c.m_p/3).to('keV')
+    elif quantity=='velocity':
+        return np.sqrt(c.G*M).to('km/s')
+    elif quantity=='time':
+        return (R/np.sqrt(c.G*M)).to('Myr')
     else:
-        return rho0(M, z, c=c, mass_def=mass_def) * theta(r, M, z, c, mass_def=mass_def)**(1.0 / (Gamma(c) - 1.0))
+        raise ValueError("Unsupported quantity")
 
-def sig2_tot(r, M, z, conc_model='diemer19', mass_def='vir', c=None):
-    if c is None:
-        c = concentration.concentration(M, mass_def, z, model=conc_model) # tabulated probably
-    R = mass_so.M_to_R(M, z, mass_def) # tabulated probably
-    rho0_by_P0 = 3*eta0(c)**-1 * R/(G*M)
-    phi0 = -1. * (c / NFWf(c))
-    phir = -1. * (c / NFWf(c)) * (np.log(1. + c*r/R) / (c*r/R))
-    theta = 1. + ((Gamma(c) - 1.) / Gamma(c)) * 3. *eta0(c)**-1 * (phi0 - phir)
-    return (1.0 / rho0_by_P0) * theta
+def filament_profile(Mv, z, cosmo, mdef = '40m', mu = 0.59, model='isothermal', r=None, **kwargs):
+    '''Calculate the virial/normalized values for different quantities of halos.
+    Parameters:
+       M: filament mass per unit length in Msun/kpc unit
+       z: redshift
+       cosmo: astropy cosmology object
+          Example: from astropy.cosmology import Planck15 as cosmo
+       mdef: overdensity mass definition
+          xxxc or xxxm (xxx is any integer), e.g. 200c. Ignored for model='collapse'. 
+       mu: mean particle weigh. Only used in tempreature.
+       model: isothermal - assume isothermal profile, see Lu+2024.
+       collapse - assume self-similar cylindrical collapse, see Aung+2024 and filament_model.pdf.
+       r: radius at which profile is calculated. Must be given as normalized unit in Rvir, i.e. r/Rvir.
+       If None, profile is calculated at 100 logarithmic points from 0.01-1Rvir.
+       Not used in collapse model right now.
+       **kwargs: for collapse solver
+    Return:
+       Rv, r, rhogas, T, P, Mgas profiles as astropy quantity array. 
+       Can use .to('xx') to convert to desired units. See astropy documentations.
+       Default units are Rv/r(kpc), rho(g/cm3), T(keV), P(keV/cm3), M(Msun/kpc)
+    '''
+    if model=='isothermal':
+        return filament_profile_isothermal(Mv, z, cosmo, mdef = mdef, mu = mu, r=r, dm=True)
+    elif model=='isothermal_nodm':
+        return filament_profile_isothermal(Mv, z, cosmo, mdef = mdef, mu = mu, r=r, dm=False)
+    elif model=='collapse':
+        return filament_profile_collapse(Mv, z, cosmo, mdef=mdef, mu = mu, **kwargs)
+    else:
+        raise ValueError("Unsupported model")
+        
+        
+def filament_profile_isothermal(Mv, z, cosmo, mdef = '40m', mu = 0.59, r=None, dm=True):
+    '''Called by filament_profile. 
+    '''
+    try:
+        if not Mv.unit.is_equivalent(u.Msun/u.kpc):
+            raise ValueError('wrong Mv unit')
+    except AttributeError:
+        Mv = Mv*u.Msun/u.kpc
+    Rv = virial_values_filament(Mv, z, cosmo, mdef=mdef, quantity='radius')
+    Tv = virial_values_filament(Mv, z, cosmo, mdef=mdef, quantity='temperature', mu=mu)
+    Mcrit = 2*Tv/(c.G*mu*c.m_p) #critical line-mass
+    if r is None:
+        r = np.logspace(-2,0)
+    r = r*Rv
+    r0 = 0.25*Rv #from Lu+2024
+    rho0 = Mcrit/(np.pi*r0**2)
+    rho = rho0/(1 + (r/r0)**2)**2
+    T = Tv*np.ones(len(r))
+    if dm:
+        fb = cosmo.Ob0/cosmo.Om0
+    else:
+        fb = 1
+    P = (fb*rho*T/(mu*c.m_p)).to('keV/cm3')
+    M = Mcrit/(1+ (r0/r)**2)
+    normm = (Mv/(Mcrit/(1+ (r0/Rv)**2))).to('')
+    return Rv.to('kpc'), r.to('kpc'), normm*fb*rho.to('g/cm3'), T.to('keV'), P.to('keV/cm3'), fb*normm*M.to('Msun/kpc')
 
-G = colossus.utils.constants.G
-Msunkpc3_2_gcm3 = 6.77e-32
+def continuity_eq_p(r,init,delta,gamma): 
+    '''Eq B6 with P'''
+    D,P,V,M=init[0],init[1],init[2],init[3]
+    Vs = V - delta*r
+    Mat = [[Vs*r,0.0,D*r,0.0],
+       [0.0,r,Vs*D*r,0.0], 
+       [-gamma*Vs*P, Vs*D,0.0,0.0],
+       [0.0,0.0,0.0,1.0]]
+    b = [[2.0*D*r-D*V],
+       [D*(r**2)/9+V*D*r*(1-delta)-M*D/3.0],
+       [D*P*(4.0-2.0*delta-2.0*gamma)],
+       [2.0*r*D]]
+    return np.linalg.solve(Mat,b).T[0]
 
-def NFWf(x):
-    return np.log(1. + x) - x/(1. + x)
+def continuity_eq_nop(r,init,delta,gamma): 
+    '''Eq B6 with no P'''
+    D,V,M=init[0],init[1],init[2]
+    Vs = V - delta*r
+    Mat = [[Vs*r,D*r,0.0],
+       [0.0,Vs*r,0.0], 
+       [0.0,0.0,1.0]]
+    b = [[2.0*D*r-D*V],
+       [(r**2)/9+V*r*(1-delta)-M/3.0],
+       [2.0*r*D]]
+    return np.linalg.solve(Mat,b).T[0]
 
-cbf = cosmo.Ob0 / cosmo.Om0
-cosmo_to_evcm3 = 4.224e-10
-eV_to_erg = 1.602e-12
+def shock_jump(D1, V1, M1, delta, lambdash, gamma):
+    '''Apply normal shock jump condition with infinite Mach, Eq B7.
+    return D2, P2, V2, M2'''
+    Vs = V1 - delta*lambdash
+    return [(gamma+1)*D1/(gamma-1),(2*D1)*(Vs)**2/(gamma+1),
+            delta*lambdash+(gamma-1)*Vs/(gamma+1),M1]
 
-def NFWM(r, M, z, c, R):
-    return M * NFWf(c*r/R) / NFWf(c)
-    
-def tcooltff_halo(radius, halo_mass, z, metallicity = 0.):
-    mass = cl.add_perh(halo_mass , cosmo.h) #Msun/h
-    Rvir = mass_so.M_to_R(mass, z, 'vir') # kpc / h
-    rads = radius * Rvir #np.logspace(np.log10(0.01*Rvir),np.log10(1.1*Rvir), 1000)
-    norm_dens, conc = rho_gas(rads, mass, z, mass_def='vir')
-    norm_temp = sig2_tot(rads, mass, z, mass_def='vir')
-    pressure = norm_temp * norm_dens
-    mu = 0.61
+def filament_profile_collapse_nonnorm(init,delta,gamma,test_incr = 0.01, inner_tol = 1e-10, r_eval=None):
+    '''Solve eq B6 and B7 in appendix and output lambda_sh, M_sh, lambda, D, P, V, M
+    '''
+    innerM, V, test_lambdash = 1, -1, 0.01
+    if r_eval is not None:
+        inner_tol_compute = np.amin(r_eval)
+        incr_compute = np.amin(np.abs(r_eval[1] - r_eval[0]))
+        if inner_tol > inner_tol_compute:
+            inner_tol = inner_tol_compute
+        if test_incr > incr_compute:
+            test_incr = incr_compute
+    nop_solution = solve_ivp(lambda r,var: continuity_eq_nop(r,var,delta,gamma), (1.00,inner_tol), init,method='Radau', t_eval = r_eval,dense_output=True)
+    while V < 0 and innerM > 0:
+        mask = nop_solution.t>=test_lambdash
+        preshock_sol = nop_solution.y[:,mask]
+        new_init = shock_jump(preshock_sol[0,-1], preshock_sol[1,-1], preshock_sol[2,-1], delta, test_lambdash, gamma)
+        if r_eval is not None:
+            t_eval = r_eval[r_eval<test_lambdash]
+        else:
+            t_eval = None
+        postshock_sol = solve_ivp(lambda r,var: continuity_eq_p(r,var,delta,gamma), (test_lambdash,inner_tol), new_init ,method='Radau',dense_output=True, t_eval = t_eval)
+        innerv = postshock_sol.y[2,-1]
+        innerM = postshock_sol.y[3,-1]
+        if innerv > 0 and innerM > 0:
+            test_lambdash = test_lambdash-test_incr*innerv/(innerv - V)
+        if innerM < 0 : 
+            test_lambdash = np.nan
+        test_lambdash += test_incr
+        if test_lambdash<0:
+            raise Error("Unknown Error")
+        V = innerv
+    t = np.r_[nop_solution.t[mask] , postshock_sol.t]
+    D = np.r_[preshock_sol[0] , postshock_sol.y[0]]
+    P = np.r_[np.zeros_like(nop_solution.t[mask]), postshock_sol.y[1]]
+    V = np.r_[preshock_sol[1] , postshock_sol.y[2]]
+    M = np.r_[preshock_sol[2] , postshock_sol.y[3]]
+    return test_lambdash, preshock_sol[2,-1], t, D, P, V, M
+
+def filament_profile_collapse(Mv, z, cosmo, mdef = '40m',mu = 0.59, dm=True, s=1.5,**kwargs):
+    '''Called by filament_profile. 
+    '''
+    try:
+        if not Mv.unit.is_equivalent(u.Msun/u.kpc):
+            raise ValueError('wrong Mv unit')
+    except AttributeError:
+        Mv = Mv*u.Msun/u.kpc
+    delta = (s+2)/3
     gamma = 5./3
-    cosmic_fraction = cosmo.Ob0/cosmo.Om0
-    density = norm_dens*Msunkpc3_2_gcm3 * u.g/u.cm**3
-    temperature = ((pressure*cosmo_to_evcm3 *eV_to_erg * u.erg/u.cm**3)* \
-                   (mu*c.m_p)/ (density * c.k_B)).to('K')
-    tcool = c.k_B*temperature / \
-    ((gamma-1)*(density*cosmic_fraction/(mu*c.m_p))* C.Lambda_units(metallicity,temperature).flatten())
-    enclosed_mass = cl.remove_perh(NFWM(rads, mass, z, conc, Rvir), cosmo.h)
-    tff = cl.remove_perh(rads, cosmo.h) *u.kpc/np.sqrt(c.G * enclosed_mass *u.Msun / (rads *u.kpc) )
-    return (tcool/tff).to('')/1.5, enclosed_mass/halo_mass
-    
-    
-def halo_mar(m, z):
-    return (0.03/cl.u.Gyr)*m*cl.u.Msun*((m/1e12)**0.14)*(1+z)**(5./2)
+    lambda_sh, M_sh, r, D, P, V, M = filament_profile_collapse_nonnorm([0.16, 0.  , 3.53],delta,gamma, r_eval = np.logspace(0,-3,num=100), **kwargs)
+    T_sh = (P/D)[P>0][0]
+    M = M*Mv/M_sh
+    Mta = M[0]/3.53
+    rhob = cosmo.Om(z) * cosmo.critical_density(z) 
+    rta = np.sqrt(Mta / (np.pi * rhob) )
+    Tv = virial_values_filament(Mv, z, cosmo, mdef=mdef, quantity='temperature', mu=mu)
+    r = r*rta
+    rho = D*rhob
+    T = (P/D)*Tv/T_sh
+    P = rho*T/(mu*c.m_p)
+    return lambda_sh*rta.to('kpc'), r.to('kpc'), rho.to('g/cm3'), T.to('keV'), P.to('keV/cm3'), M.to('Msun/kpc')
 
-def filament_T(m, z, facc=1):
-    mfil = (halo_mar(m, z)/3) /cl.virial_values(m, z, cosmo, mdef='200c',quantity='velocity')
-    return cl.c.G*mfil*(0.59*cl.c.m_p)
+def tcool(T, rho, metallicity = 0., gamma=5./3, mu=0.59):
+    '''Return cooling time'''
+    return T.to('keV',  equivalencies=u.temperature_energy()) / \
+    ((gamma-1)*(rho/(mu*c.m_p))* C.Lambda_units(metallicity,T.to('K', equivalencies=u.temperature_energy()))) 
+
+def tff(rhobar):
+    '''Return freefall time of cylindar'''
+    return np.sqrt(3*np.pi/(32*c.G*rhobar))
